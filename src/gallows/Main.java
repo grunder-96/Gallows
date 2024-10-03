b54237df06f6df00205165c14996c1c2fb59ffaa
Grigory Taran <grunder-96@mail.ru>
2024-10-03 12:13:19 +0300

    refactor: remove unnecessary instances in the main method

--- src/gallows/Main.java
+++ src/gallows/Main.java
@@ -1,14 +1,8 @@
 package gallows;
 
-import gallows.validator.LauncherSelectionValidator;
-import gallows.validator.Validator;
-
 public class Main {
 
     public static void main(String[] args) {
-        Reader reader = new Reader();
-        Validator validator = new LauncherSelectionValidator();
-        GameLauncher launcher = new GameLauncher(reader, validator);
-        launcher.launch();
+        new GameLauncher().launch();
     }
 }
