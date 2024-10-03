4fb69401be61843b7bd6147d79b7797c28087495
Grigory Taran <grunder-96@mail.ru>
2024-10-03 10:24:38 +0300

    fix: now the validator recognizes all letters of the russian alphabet

--- src/gallows/validator/LetterInputValidator.java
+++ src/gallows/validator/LetterInputValidator.java
@@ -4,6 +4,6 @@ public class LetterInputValidator implements Validator<String> {
 
     @Override
     public boolean validate(String input) {
-        return input.length() == 1 && input.matches("[а-яА-я]");
+        return input.length() == 1 && input.matches("[а-яА-яёЁ]");
     }
 }
